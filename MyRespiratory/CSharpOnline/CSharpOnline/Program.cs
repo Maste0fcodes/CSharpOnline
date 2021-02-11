072d1bb6084748bd6acc6529988a21461c48fbab
Gabriel Lee, Clay <52018774+Maste0fcodes@users.noreply.github.com>
2021-02-11 17:51:38 -0600

    Add files via upload

---
+++ MyRespiratory/CSharpOnline/CSharpOnline/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace CSharpOnline
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is my first sentece");
+            Console.WriteLine("I am new to C#");
+            Console.WriteLine("This course is cool");
+        }
+    }
+}
