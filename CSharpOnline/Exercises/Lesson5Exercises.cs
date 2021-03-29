8f22553c8ce942121bdb52cd9c4554a0950bc7ba
Gabriel Lee Clay <gabrielclay14@gmail.com>
2021-03-29 13:00:55 -0500

    Lesson 5 - Exercises

---
+++ CSharpOnline/Exercises/Lesson5Exercises.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharpOnline.Exercises
+{
+    class Lesson5Exercises
+    {
+        // Output Methods that printed out the number 29
+        int _val3 = 0b11101;
+        int _val = 29;
+        int _val2 = 0x1D;
+
+        byte _myByteValue = 20;
+        int _myIntValue = 2;
+        long _myLongValue = 1234577L;
+        short _myShortValue = 12;
+        double _myDoubleValue = 12.24D;
+        float _myFloatValue = 111.222F;
+
+
+        void MyExplicit()
+        {
+            double someValue = (double)_myIntValue;
+            Console.WriteLine("longVariable");
+        }
+
+        void MyImplicit()
+        {
+            byte converted = (byte)_myByteValue;
+            Console.WriteLine("shortVaraible");
+        }
+
+
+    }
+}
