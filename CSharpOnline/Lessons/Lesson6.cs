f790d3d27bec0353a3fd4d0581afad094273b446
Gabriel Lee Clay <gabrielclay14@gmail.com>
2021-03-29 12:59:56 -0500

    Lesson 6 - strings & more

---
+++ CSharpOnline/Lessons/Lesson6.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharpOnline.Lessons
+{
+    /*This class goes over strings & more 
+     Lesson 6 - strings & more*/
+    class Lesson6
+    {
+
+        string _myString = "Hello World";
+
+        // Trim
+        public void MyTrimExample()
+        {
+            string _myStr = " Space ";
+            // Before Trim
+            Console.WriteLine(_myStr);
+            // trim
+            string mytrim = _myStr.Trim();
+            // After trim
+            Console.WriteLine(mytrim);
+        }
+
+        // Equals
+        public void MyEqualsExample()
+        {
+            bool isExampleEqual = _myString.Equals("World");
+            Console.WriteLine(isExampleEqual);
+        }
+
+        // To upper & to lower
+        public void MyLowerUpperExample()
+        {
+            // Nested method - Method called through another method
+            Console.WriteLine(_myString.ToLower());
+            Console.WriteLine(_myString.ToUpper());
+        }
+
+        // Length
+        public void myStringLength()
+        {
+            Console.WriteLine(_myString.Length);
+        }
+
+        // Concatenation - with two prameters 
+        public void MyJoinedStrings(String value1, string value2)
+        {
+            Console.WriteLine(value1 + value2);
+        }
+
+        // String builder - with two prameters, return string 
+        public string MyStringBuilder(string food, int amount)
+        {
+            StringBuilder builder = new StringBuilder();
+            builder.Append("My favorite food is ");
+            builder.Append(food).Append(" and i ate ")
+                .Append(amount).Append(" plates of it.");
+            // returns the value out of the method
+            return builder.ToString();
+        }
+
+        // Char - single quote 
+        public void MyExampleChar()
+        {
+            char myChar = 't';
+            char myOther = '\u00A7';
+            char copyright = '\u00A9';
+            Console.WriteLine(myOther + " " + copyright);
+            Console.WriteLine(myChar);
+        }
+
+        // Escape Sequence
+        public string MyEscapeExample()
+        {
+            return " That\'s a cool toy. \tCan I\t play with it? ";
+        }
+
+        public void MyPlaceHolder(string game, string type)
+        {
+            Console.WriteLine("My favorite game {0} is a {1}", game, type);
+        }
+
+       
+    } //end clase
+} //end namespace
--- CSharpOnline/Program.cs
+++ CSharpOnline/Program.cs
@@ -5,16 +5,33 @@ namespace CSharpOnline
 {
     class Program
     {
-        static void Main(string[] args)
+
+        static void Main(string [] args)
         {
-            MyLesson5Examples();
+            MyLesson6Examples();
+            //MyLesson5Examples();
            // Console.WriteLine("Hello World!");
            // House _myHouse = new House();
         }
 
+        static void MyLesson6Examples()
+        {
+            Lesson6 mySix = new Lesson6();
+            mySix.MyTrimExample();
+            mySix.MyEqualsExample();
+            mySix.MyLowerUpperExample();
+            mySix.myStringLength();
+            mySix.MyJoinedStrings("apple" , "orange");
+            string food = mySix.MyStringBuilder("pizza", 2);
+            Console.WriteLine(food);
+            mySix.MyExampleChar();
+            mySix.MyEscapeExample();
+            mySix.MyPlaceHolder("Galaga", "shooter");
+        }
+
         static void MyLesson5Examples()
         {
-            Lesson5 myFive = new Lesson5();
+             Lesson5 myFive = new Lesson5();
             myFive.ConvertFloatToInt();
             myFive.LongFromInt();
         }
